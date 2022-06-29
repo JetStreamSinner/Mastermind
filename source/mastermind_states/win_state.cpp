1698d46381afdf51f2ebd1b9df68548bfc50e18f
Jetstream <bigalex934@gmail.com>
2022-06-29 15:13:47 +0300

    Implement win state

--- source/mastermind_states/win_state.cpp
+++ source/mastermind_states/win_state.cpp
@@ -1,9 +1,12 @@
 #include "win_state.h"
+#include "../game_area.h"
+#include "../abstract_user_request_acceptor.h"
+#include "start_state.h"
 
 bool WinState::exec(std::shared_ptr<GameArea>& area, std::unique_ptr<AbstractUserRequestAcceptor> request_acceptor) {
-
+    return true;
 }
 
 std::unique_ptr<AbstractMastermindState> WinState::nextState() {
-
+    return std::make_unique<StartState>();
 }
