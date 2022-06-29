26c2f50a7440b2cd906a931498b3db7a8de3df4f
Jetstream <bigalex934@gmail.com>
2022-06-29 14:54:33 +0300

    Implement start state

--- source/mastermind_states/start_state.cpp
+++ source/mastermind_states/start_state.cpp
@@ -1,8 +1,16 @@
 #include "start_state.h"
 #include "guess_state.h"
 #include "../game_area.h"
+#include "../abstract_user_request_acceptor.h"
+#include "../guess_checker.h"
 
 bool StartState::exec(std::shared_ptr<GameArea>& area, std::unique_ptr<AbstractUserRequestAcceptor> request_acceptor) {
+    const std::size_t suggestions_count = request_acceptor->requestSuggestionsCount();
+    const SequenceRow target_row = request_acceptor->requestTargetRow();
+
+    auto guess_checker = std::make_unique<GuessChecker>(target_row);
+    area = std::make_shared<GameArea>(suggestions_count, std::move(guess_checker));
+    return true;
 }
 
 std::unique_ptr<AbstractMastermindState> StartState::nextState() {
