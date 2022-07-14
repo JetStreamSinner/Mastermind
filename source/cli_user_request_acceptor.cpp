1bc62a66fc1595185577581e6c3535bced2c140e
Jetstream <bigalex934@gmail.com>
2022-07-14 22:32:27 +0300

    Remove unnecessary print

--- source/cli_user_request_acceptor.cpp
+++ source/cli_user_request_acceptor.cpp
@@ -8,7 +8,6 @@ SequenceRow sequenceRowFromString(const std::string &str) {
     std::stringstream ss(str);
     SequenceRow out;
     std::transform(std::istream_iterator<int>(ss), std::istream_iterator<int>(), std::back_inserter(out), [](int val) {
-        std::cout << "Input val: " << val << std::endl;
         return static_cast<Color>(val);
     });
     return std::move(out);
