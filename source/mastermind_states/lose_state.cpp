2cf9c137654a60d2c6e121feae932a5ae2652451
Jetstream <bigalex934@gmail.com>
2022-07-20 20:17:05 +0300

    Implement pimpl for lose state

--- source/mastermind_states/lose_state.cpp
+++ source/mastermind_states/lose_state.cpp
@@ -3,11 +3,31 @@
 #include "../interfaces/abstract_user_request_acceptor.h"
 #include "start_state.h"
 
-bool LoseState::exec(std::shared_ptr<GameArea> &area, const std::unique_ptr<AbstractUserRequestAcceptor> &request_acceptor) {
+class LoseState::LoseStateImpl {
+public:
+    bool exec(std::shared_ptr<GameArea> &area, const std::unique_ptr<AbstractUserRequestAcceptor> &request_acceptor);
+    std::unique_ptr<AbstractMastermindState> nextState();
+};
+
+bool LoseState::LoseStateImpl::exec(std::shared_ptr<GameArea> &area, const std::unique_ptr<AbstractUserRequestAcceptor> &request_acceptor) {
     request_acceptor->writeMessage("You lose!");
     return true;
 }
 
-std::unique_ptr<AbstractMastermindState> LoseState::nextState() {
+std::unique_ptr<AbstractMastermindState> LoseState::LoseStateImpl::nextState() {
     return std::make_unique<StartState>();
 }
+
+LoseState::LoseState() : _impl(std::make_unique<LoseState::LoseStateImpl>()) {
+
+}
+
+bool LoseState::exec(std::shared_ptr<GameArea> &area, const std::unique_ptr<AbstractUserRequestAcceptor> &request_acceptor) {
+    return _impl->exec(area, request_acceptor);
+}
+
+std::unique_ptr<AbstractMastermindState> LoseState::nextState() {
+    return _impl->nextState();
+}
+
+LoseState::~LoseState() = default;
--- source/mastermind_states/lose_state.h
+++ source/mastermind_states/lose_state.h
@@ -4,6 +4,11 @@
 
 class LoseState : public AbstractMastermindState {
 public:
+    explicit LoseState();
     bool exec(std::shared_ptr<GameArea> &area, const std::unique_ptr<AbstractUserRequestAcceptor> &request_acceptor) final;
     std::unique_ptr<AbstractMastermindState> nextState() final;
+    ~LoseState() override;
+private:
+    class LoseStateImpl;
+    std::unique_ptr<LoseStateImpl> _impl;
 };
